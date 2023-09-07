2e708fb95b2bd7da8947307eb2655906db4246f2
denvil86 <denisvilante@gmail.com>
2023-09-07 17:31:24 -0300

    refazendo

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 //iniciar projeto
 console.log ("vamos começar");
 //variaveis
-let NivelDoHeroi;
-const NomeDoHeroi = "Denvil";
-const XpDoHeroi = 6999;
+let NivelDoHeroi = "Ouro";
+let NomeDoHeroi = "Denvil";
+let XpDoHeroi = 6999;
 
-if (XpDoHeroi < 1000){NivelDoHeroi = "Ferro"};
-else if (XpDoHeroi >= 1001 && XpDoHeroi <= 2000) {NivelDoHeroi = "Bronze"};
-else if (XpDoHeroi >= 2001 && XpDoHeroi <= 5000) {NivelDoHeroi = "Prata"};
-else if (XpDoHeroi >= 6001 && XpDoHeroi <= 7000) {NivelDoHeroi = "Ouro"};
-else if (XpDoHeroi >= 7001 && XpDoHeroi <= 8000) {NivelDoHeroi = "Platina"};
-else if (XpDoHeroi >= 8001 && XpDoHeroi <= 9000) {NivelDoHeroi = "Ascendente"};
-else if (XpDoHeroi >= 9001 && XpDoHeroi <= 10000) {NivelDoHeroi = "Imortal"};
-else (XpDoHeroi >= 10001) {NivelDoHeroi = "Radiante"};
-
+if (XpDoHeroi < 1000){
+    console.log (NivelDoHeroi = "Ferro ")
+}
+else if (XpDoHeroi >= 1001 && XpDoHeroi <= 2000){
+   console.log (NivelDoHeroi = "Bronze")
+};
+else if (XpDoHeroi >= 2001 && XpDoHeroi <= 5000){
+   console.log (NivelDoHeroi = "Prata")
+};
+else if (XpDoHeroi >= 6001 && XpDoHeroi <= 7000){
+    NivelDoHeroi = "Ouro"
+};
+else if (XpDoHeroi >= 7001 && XpDoHeroi <= 8000) {
+    NivelDoHeroi = "Platina"
+};
+else if (XpDoHeroi >= 8001 && XpDoHeroi <= 9000) {
+    NivelDoHeroi = "Ascendente"
+};
+else if (XpDoHeroi >= 9001 && XpDoHeroi <= 10000) {
+    NivelDoHeroi = "Imortal"
+};
 
 // Saida
 console.log('O Heroi de nome'{NomeDoHeroi} 'esta no nivel de'{NivelDoHeroi});
