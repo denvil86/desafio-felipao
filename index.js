dc519bb56d83f17049568b9adedfd467e9987296
denvil86 <denisvilante@gmail.com>
2023-09-07 19:05:26 -0300

    enviando em switch

--- index.js
+++ index.js
@@ -5,27 +5,32 @@ let NivelDoHeroi = "Ouro";
 let NomeDoHeroi = "Denvil";
 let XpDoHeroi = 6999;
 
-if (XpDoHeroi < 1000){
-    console.log (NivelDoHeroi = "Ferro ")
-}
-else if (XpDoHeroi >= 1001 && XpDoHeroi <= 2000){
-   console.log (NivelDoHeroi = "Bronze")
-};
-else if (XpDoHeroi >= 2001 && XpDoHeroi <= 5000){
-   console.log (NivelDoHeroi = "Prata")
-};
-else if (XpDoHeroi >= 6001 && XpDoHeroi <= 7000){
-    NivelDoHeroi = "Ouro"
-};
-else if (XpDoHeroi >= 7001 && XpDoHeroi <= 8000) {
-    NivelDoHeroi = "Platina"
-};
-else if (XpDoHeroi >= 8001 && XpDoHeroi <= 9000) {
-    NivelDoHeroi = "Ascendente"
-};
-else if (XpDoHeroi >= 9001 && XpDoHeroi <= 10000) {
-    NivelDoHeroi = "Imortal"
-};
-
+switch (true){
+    case XpDoHeroi < 1000: 
+        NivelDoHeroi = "Ferro ";
+        break;
+    case XpDoHeroi >= 1001 && XpDoHeroi <= 2000:
+        NivelDoHeroi = "Bronze";
+        break;
+    case XpDoHeroi >= 2001 && XpDoHeroi <= 5000:
+        NivelDoHeroi = "Prata";
+        break;
+    case XpDoHeroi >= 6001 && XpDoHeroi <= 7000:
+        NivelDoHeroi = "Ouro";
+        break;
+    case XpDoHeroi >= 7001 && XpDoHeroi <= 8000:
+        NivelDoHeroi = "Platina";
+        break;
+    case XpDoHeroi >= 8001 && XpDoHeroi <= 9000:
+        NivelDoHeroi = "Ascendente";
+        break;
+    case XpDoHeroi >= 9001 && XpDoHeroi <= 10000:
+    NivelDoHeroi = "Imortal";
+        break;
+    default:
+    NivelDoHeroi = "Radiante";
+    break;
+     
+    
 // Saida
 console.log('O Heroi de nome'{NomeDoHeroi} 'esta no nivel de'{NivelDoHeroi});
